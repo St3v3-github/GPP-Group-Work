f6f1d8837df22776702cdde299fe93993c544b0f
Isaac <isaac2.turpin@live.uwe.ac.uk>
2023-04-20 12:36:59 +0100

    added basic enemy and updated "SampleScene"

---
+++ Assets/Enemy Scripts/BasicEnemyDamage.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyDamage : MonoBehaviour
+{
+    public float health = 2;
+
+    PlayerController playerController;
+    public GameObject sword;
+    public GameObject basicEnemy;
+    public bool hasAttacked = false;
+    public float delay = 1;
+
+    [SerializeField] private Renderer myModel;
+    public Material defaultMat;
+    public Material redMat;
+
+    private void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Debug.Log("dead");
+            basicEnemy.SetActive(false);
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider Sword = sword.GetComponent<Collider>();
+        if (other == Sword && playerController.isAttacking && !hasAttacked)
+        {
+            hasAttacked = true;
+            myModel.material = redMat;
+            Debug.Log("Working basic");
+            Invoke(nameof(CheckAttack), delay);
+        }
+    }
+    public void CheckAttack()
+    {
+        TakeDamage(1);
+        hasAttacked = false;
+        myModel.material = defaultMat;
+
+    }
+}
